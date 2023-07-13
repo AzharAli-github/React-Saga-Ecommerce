d601936056b4dfc6dfaa962e3ffafcdcb7597149
AzharAli-github <azharborithic11@gmail.com>
2023-07-13 18:33:01 +0500

    saga

---
+++ src/components/Cart.js
@@ -0,0 +1,52 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
+const Cart = () => {
+  const cartData = useSelector((state) => state.cartData);
+  let amount =
+    cartData.length &&
+    cartData.map((item) => item.price).reduce((prev, next) => prev + next);
+  console.warn(amount);
+  return (
+    <div>
+      <Link to="/">Go to Products Link</Link>
+      <h1>Cart Page</h1>
+      <div className="cart-page-container">
+        <table>
+          <tr>
+            <td>ID</td>
+            <td>Name</td>
+            <td>Email</td>
+          </tr>
+          {cartData.map((item) => (
+            <tr key={item.key}>
+              <td>{item.postId}</td>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+            </tr>
+          ))}
+        </table>
+        <div className="price-details">
+          <div className="adjust-price">
+            <span>Amount</span>
+            <span>{amount}</span>
+          </div>
+          <div className="adjust-price">
+            <span>Discount</span>
+            <span>{amount / 10}</span>
+          </div>
+          <div className="adjust-price">
+            <span>Tax</span>
+            <span>000</span>
+          </div>
+          <div className="adjust-price">
+            <span>Total</span>
+            <span>{amount - amount / 10}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
